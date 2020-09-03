70631884461cd0f22883253a78ea51e65aff8d7e
Angel <albertoangelaguilera@gmail.con>
2020-09-03 16:06:39 -0500

    added autoincrement index

--- src/core/forms-creation/forms-creation.module.ts
+++ src/core/forms-creation/forms-creation.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { FormsCreationController } from './forms-creation.controller';
 import { FormsCreationService } from './forms-creation.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, getConnectionToken } from '@nestjs/mongoose';
 import { FormCreationSchema } from 'src/core/forms-creation/schemas/form.schema';
-
+import { Connection } from 'mongoose';
+import * as AutoIncrementFactory from 'mongoose-sequence';
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'Form', schema: FormCreationSchema }])],
+    imports: [ MongooseModule.forFeatureAsync([
+      {
+        name: 'Form',
+        useFactory: async (connection: Connection) => {
+          const schema = FormCreationSchema;
+          const AutoIncrement = AutoIncrementFactory(connection);
+          schema.plugin(AutoIncrement, {inc_field: '_id'});
+          return schema;
+        },
+        inject: [getConnectionToken()],
+      },
+    ]),],
     controllers: [FormsCreationController],
     providers: [FormsCreationService],
   })
